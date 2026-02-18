83f62a4fab55d1dce8d488c9ccb72a2c20d02af7
Andrei Zarechnev <andrei@zarechnev.space>
2026-02-19 00:43:41 +0300

    chore: configure Sanity Studio deploy host and appId

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,7 +5,9 @@ export default defineCliConfig({
     projectId: '7framccn',
     dataset: 'production'
   },
+  studioHost: 'zarechnev-space-studio',
   deployment: {
+    appId: 'vpyb2dd4kzxj0ezcwl1i33h1',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
